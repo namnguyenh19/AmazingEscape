15df88a3c620b29987f366d36789ef4b33070297
Nam Nguyen <namn1@student.unimelb.edu.au>
2017-05-28 02:38:44 +1000

    finish check corner and deadend method

--- mycontroller/View.java
+++ mycontroller/View.java
@@ -13,11 +13,12 @@ import java.util.HashMap;
 public class View {
     private HashMap<Coordinate, MapTile> curView;
     private WorldSpatial.Direction curDir;
-    private Coordinate currentPosition;
+    private Coordinate curPos;
     private boolean canUTurn;
     private boolean canThreePoint;
-    // How many minimum units the wall is away from the player.
-    private int wallSensitivity = 2;
+
+    // distance to each direction the car can see
+    public static final int VIEW_SQUARE = 3;
 
 
     public View(HashMap<Coordinate, MapTile> view, WorldSpatial.Direction dir){
@@ -27,20 +28,17 @@ public class View {
 
     /**
      * Check if you have a wall in front of you!
-     * @param orientation the orientation we are in based on WorldSpatial
-     * @param curView what the car can currently see
-     * @return
      */
-    private boolean checkWallAhead(WorldSpatial.Direction orientation, HashMap<Coordinate, MapTile> curView){
-        switch(orientation){
+    private boolean checkWallAhead(){
+        switch(this.curDir){
             case EAST:
-                return checkEast();
+                return checkEast(curPos);
             case NORTH:
-                return checkNorth();
+                return checkNorth(curPos);
             case SOUTH:
-                return checkSouth();
+                return checkSouth(curPos);
             case WEST:
-                return checkWest();
+                return checkWest(curPos);
             default:
                 return false;
 
@@ -49,47 +47,66 @@ public class View {
 
     /**
      * Check if the wall is on your left hand side given your orientation
-     * @param orientation
-     * @param curView
-     * @return
      */
-    private boolean checkFollowingWall(WorldSpatial.Direction orientation, HashMap<Coordinate, MapTile> curView) {
+    private boolean checkFollowingWall() {
 
-        switch(orientation){
+        switch(this.curDir){
             case EAST:
-                return checkNorth();
+                return checkNorth(curPos);
             case NORTH:
-                return checkWest();
+                return checkWest(curPos);
             case SOUTH:
-                return checkEast();
+                return checkEast(curPos);
             case WEST:
-                return checkSouth();
+                return checkSouth(curPos);
             default:
                 return false;
         }
 
     }
 
-    /*
-     *
+    /**
+     * Check if there is a corner ahead
      */
     public boolean checkCornerAhead(){
-        return false;
+
+        switch (this.curDir){
+            case EAST:
+                return checkCornerEast();
+            case NORTH:
+                return checkCornerNorth();
+            case SOUTH:
+                return checkCornerSouth();
+            case WEST:
+                return checkCornerWest();
+            default:
+                return false;
+        }
     }
 
-    /*
-     *
+    /**
+     * Check if there is a dead end ahead
      */
     public boolean checkDeadEnd(){
-        return false;
+        switch (this.curDir){
+            case EAST:
+                return checkEndEast();
+            case NORTH:
+                return checkEndNorth();
+            case SOUTH:
+                return checkEndSouth();
+            case WEST:
+                return checkEndWest();
+            default:
+                return false;
+        }
     }
 
-    /*
-     *
+    /**
+     * Check if there is enough space to do U Turn and/or Three Point
      */
     public void checkSpace(){
-        this.canThreePoint = false;
-        this.canUTurn = false;
+
     }
 
     public ArrayList<Path> getPaths(){
@@ -97,17 +114,160 @@ public class View {
     }
 
     /**
+     * Method below checks for upcoming dead end given respective orientation of the car
+     * It checks if there is wall ahead, if so check responding walls on two sides
+     */
+
+    private boolean checkEndEast(){
+        for (int i = 1; i <= VIEW_SQUARE; i++){
+            Coordinate coor = new Coordinate(this.curPos.x + i, this.curPos.y);
+            MapTile tile = this.curView.get(coor);
+            if(tile.getName().equals("Wall")){
+                MapTile leftTile = this.curView.get(new Coordinate(coor.x-1, coor.y+1));
+                MapTile rightTile = this.curView.get(new Coordinate(coor.x-1, coor.y -1));
+                checkTwoSide(leftTile, rightTile);
+            }
+        }
+        return false;
+    }
+
+    private boolean checkEndWest(){
+        for (int i = 1; i <= VIEW_SQUARE; i++){
+            Coordinate coor = new Coordinate(this.curPos.x - i, this.curPos.y);
+            MapTile tile = this.curView.get(coor);
+            if(tile.getName().equals("Wall")){
+                MapTile leftTile = this.curView.get(new Coordinate(coor.x+1, coor.y-1));
+                MapTile rightTile = this.curView.get(new Coordinate(coor.x+1, coor.y+1));
+                checkTwoSide(leftTile, rightTile);
+            }
+        }
+        return false;
+    }
+
+    private boolean checkEndNorth(){
+        for (int i = 1; i <= VIEW_SQUARE; i++){
+            Coordinate coor = new Coordinate(this.curPos.x, this.curPos.y+i);
+            MapTile tile = this.curView.get(coor);
+            if(tile.getName().equals("Wall")){
+                MapTile leftTile = this.curView.get(new Coordinate(coor.x-1, coor.y-1));
+                MapTile rightTile = this.curView.get(new Coordinate(coor.x+1, coor.y-1));
+                checkTwoSide(leftTile, rightTile);
+            }
+        }
+        return false;
+    }
+
+    private boolean checkEndSouth(){
+        for (int i = 1; i <= VIEW_SQUARE; i++){
+            Coordinate coor = new Coordinate(this.curPos.x, this.curPos.y-i);
+            MapTile tile = this.curView.get(coor);
+            if(tile.getName().equals("Wall")){
+                MapTile leftTile = this.curView.get(new Coordinate(coor.x+1, coor.y+1));
+                MapTile rightTile = this.curView.get(new Coordinate(coor.x-1, coor.y+1));
+                checkTwoSide(leftTile, rightTile);
+            }
+        }
+        return false;
+    }
+
+    private boolean checkTwoSide(MapTile l, MapTile r){
+        if(l.getName().equals("Wall") && r.getName().equals("Wall")){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Method below checks for upcoming corner given respective orientation of the car
+     *  It checks if there is wall to the left, if so, check the tile to the wall's right
+     *  if that's not a wall, then there is a corner
+     */
+    private boolean checkCornerEast(){
+        for(int i = 0; i < VIEW_SQUARE; i++){
+            Coordinate coor = new Coordinate(this.curPos.x + i, this.curPos.y);
+            // check North with specific coordinate of a Wall
+            for(int j = 0; j < VIEW_SQUARE; j++){
+                MapTile tile = this.curView.get(new Coordinate(coor.x, coor.y+j));
+                if(tile.getName().equals("Wall")){
+                    MapTile nextTile = this.curView.get(new Coordinate(coor.x+1, coor.y+j));
+                    if(!nextTile.getName().equals("Wall")){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean checkCornerWest(){
+        for(int i = 0; i < VIEW_SQUARE; i++){
+            Coordinate coor = new Coordinate(this.curPos.x - i, this.curPos.y);
+            // check North with specific coordinate of a Wall
+            for(int j = 0; j < VIEW_SQUARE; j++){
+                MapTile tile = this.curView.get(new Coordinate(coor.x, coor.y-j));
+                if(tile.getName().equals("Wall")){
+                    MapTile nextTile = this.curView.get(new Coordinate(coor.x-1, coor.y-j));
+                    if(!nextTile.getName().equals("Wall")){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean checkCornerNorth(){
+        for(int i = 0; i < VIEW_SQUARE; i++){
+            Coordinate coor = new Coordinate(this.curPos.x, this.curPos.y + i);
+            // check North with specific coordinate of a Wall
+            for(int j = 0; j < VIEW_SQUARE; j++){
+                MapTile tile = this.curView.get(new Coordinate(coor.x-j, coor.y));
+                if(tile.getName().equals("Wall")){
+                    MapTile nextTile = this.curView.get(new Coordinate(coor.x-j, coor.y+1));
+                    if(!nextTile.getName().equals("Wall")){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean checkCornerSouth(){
+        for(int i = 0; i < VIEW_SQUARE; i++){
+            Coordinate coor = new Coordinate(this.curPos.x, this.curPos.y - i);
+            // check North with specific coordinate of a Wall
+            for(int j = 0; j < VIEW_SQUARE; j++){
+                MapTile tile = this.curView.get(new Coordinate(coor.x+j, coor.y));
+                if(tile.getName().equals("Wall")){
+                    MapTile nextTile = this.curView.get(new Coordinate(coor.x+j, coor.y-1));
+                    if(!nextTile.getName().equals("Wall")){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * Method below just iterates through the list and check in the correct coordinates.
      * i.e. Given your current position is 10,10
-     * checkEast will check up to wallSensitivity amount of tiles to the right.
-     * checkWest will check up to wallSensitivity amount of tiles to the left.
-     * checkNorth will check up to wallSensitivity amount of tiles to the top.
-     * checkSouth will check up to wallSensitivity amount of tiles below.
+     * checkEast will check up to VIEW_SQUARE -1 amount of tiles to the right.
+     * checkWest will check up to VIEW_SQUARE -1 amount of tiles to the left.
+     * checkNorth will check up to VIEW_SQUARE -1 amount of tiles to the top.
+     * checkSouth will check up to VIEW_SQUARE -1 amount of tiles below.
      */
-    public boolean checkEast(){
+    private boolean checkEast(Coordinate coor){
         // Check tiles to my right
-        for(int i = 0; i <= wallSensitivity; i++){
-            MapTile tile = this.curView.get(new Coordinate(currentPosition.x+i, currentPosition.y));
+        for(int i = 0; i < VIEW_SQUARE; i++){
+            MapTile tile = this.curView.get(new Coordinate(coor.x+i, coor.y));
             if(tile.getName().equals("Wall")){
                 return true;
             }
@@ -115,10 +275,10 @@ public class View {
         return false;
     }
 
-    public boolean checkWest(){
+    private boolean checkWest(Coordinate coor){
         // Check tiles to my left
-        for(int i = 0; i <= wallSensitivity; i++){
-            MapTile tile = this.curView.get(new Coordinate(currentPosition.x-i, currentPosition.y));
+        for(int i = 0; i < VIEW_SQUARE; i++){
+            MapTile tile = this.curView.get(new Coordinate(coor.x-i, coor.y));
             if(tile.getName().equals("Wall")){
                 return true;
             }
@@ -126,10 +286,10 @@ public class View {
         return false;
     }
 
-    public boolean checkNorth(){
+    private boolean checkNorth(Coordinate coor){
         // Check tiles to towards the top
-        for(int i = 0; i <= wallSensitivity; i++){
-            MapTile tile = this.curView.get(new Coordinate(currentPosition.x, currentPosition.y+i));
+        for(int i = 0; i < VIEW_SQUARE; i++){
+            MapTile tile = this.curView.get(new Coordinate(coor.x, coor.y+i));
             if(tile.getName().equals("Wall")){
                 return true;
             }
@@ -137,10 +297,10 @@ public class View {
         return false;
     }
 
-    public boolean checkSouth(){
+    private boolean checkSouth(Coordinate coor){
         // Check tiles towards the bottom
-        for(int i = 0; i <= wallSensitivity; i++){
-            MapTile tile = this.curView.get(new Coordinate(currentPosition.x, currentPosition.y-i));
+        for(int i = 0; i < VIEW_SQUARE; i++){
+            MapTile tile = this.curView.get(new Coordinate(coor.x, coor.y-i));
             if(tile.getName().equals("Wall")){
                 return true;
             }
