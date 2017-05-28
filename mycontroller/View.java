5ff4d25ed8aef1ec382a8993f26cadb105a0c622
Nam Nguyen <namn1@student.unimelb.edu.au>
2017-05-28 16:02:37 +1000

    fix checking dead end

--- mycontroller/View.java
+++ mycontroller/View.java
@@ -6,6 +6,7 @@ import world.WorldSpatial;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by NamNguyen1 on 27/5/17.
@@ -14,9 +15,27 @@ public class View {
     private HashMap<Coordinate, MapTile> curView;
     private WorldSpatial.Direction curDir;
     private Coordinate curPos;
+
+    public boolean isCanUTurn() {
+        return canUTurn;
+    }
+
+    public boolean isCanThreePoint() {
+        return canThreePoint;
+    }
+
+    public boolean isTurnLeft() {
+        return turnLeft;
+    }
+
+    public boolean isTurnRight() {
+        return turnRight;
+    }
+
     private boolean canUTurn;
     private boolean canThreePoint;
     private boolean turnLeft;
+    private boolean turnRight;
 
     // distance to each direction the car can see
     public static final int VIEW_SQUARE = 3;
@@ -106,20 +125,6 @@ public class View {
         }
     }
 
-    /**
-     * Check if there is enough space to do U Turn and/or Three Point
-     */
-    public boolean checkSpace() {
-        // We need at least two tiles ahead
-        for (int i = 1; i < 3; i++) {
-            Coordinate coor = new Coordinate(this.curPos.x + i, this.curPos.y);
-            MapTile tile = this.curView.get(coor);
-            if(tile.getName().equals("Wall")) {
-                return false;
-            }
-        }
-        return true;
-    }
 
     public ArrayList<Path> getPaths() {
         return null;
@@ -134,19 +139,55 @@ public class View {
      */
 
     private int checkSpace(){
+        int spaceLeft = 0;
+        int spaceRight = 0;
+        switch (this.curDir){
+            case EAST:
+                spaceLeft = sideSpace(1, false);
+                spaceRight = sideSpace(-1, false);
+                break;
+            case WEST:
+                spaceLeft = sideSpace(1, false);
+                spaceRight = sideSpace(-1, false);
+                break;
+            case NORTH:
+                spaceLeft = sideSpace(1, true);
+                spaceRight = sideSpace(-1, true);
+                break;
+            case SOUTH:
+                spaceLeft = sideSpace(1, true);
+                spaceRight = sideSpace(-1, true);
+                break;
+        }
 
+        if (spaceLeft >= 1){
+            canThreePoint = true;
+            turnLeft = true;
+        }
     }
 
     /**
      * Check how much space to a side of the car
      * @param isLeft whether we're checking the left or right side of the car
-     * @param orient whether car is facing EAST/WEST or NORTH/EAST
+     * @param vertical whether car is facing EAST/WEST or NORTH/EAST
      * @return number of empty tile(s)
      */
-    private int sideSpace(int isLeft, int orient){
+    private int sideSpace(int isLeft, boolean vertical){
         int count = 0;
+        Coordinate coor;
         for(int i = 1; i <= VIEW_SQUARE; i++){
-            Coordinate coor =
+            //if car is in NORTH/SOUTH
+            if (vertical){
+                coor = new Coordinate(this.curPos.x - isLeft*i, this.curPos.y);
+            }
+            else {
+                coor = new Coordinate(this.curPos.x, this.curPos.y + isLeft*i);
+            }
+
+            MapTile tile = this.curView.get(coor);
+            if (!checkWall(tile)){
+                count += 1;
+            }
         }
 
         return count;
@@ -220,26 +261,32 @@ public class View {
             Coordinate coor = new Coordinate(this.curPos.x, this.curPos.y+i);
             MapTile tile = this.curView.get(coor);
             if(tile.getName().equals("Wall")){
-                Coordinate leftMost = coor;
-                Coordinate rightMost = coor;
+
+                MapTile leftSide = this.curView.get(new Coordinate(coor.x-1, coor.y-1));
+                MapTile rightSide = this.curView.get(new Coordinate(coor.x+1, coor.y-1));
 
                 //get how wide the wall in front is
-                for(int j = 1; j < VIEW_SQUARE; j++){
+                for(int j = 0; j < VIEW_SQUARE; j++){
                     MapTile leftTile = this.curView.get(new Coordinate(coor.x-j, coor.y));
                     MapTile rightTile = this.curView.get(new Coordinate(coor.x+j, coor.y));
 
-                    if(checkWall(leftTile)){
-                        leftMost = new Coordinate(coor.x-j, coor.y);
+                    //check the tile directly below leftMost and rightMost, they must not be a wall
+                    MapTile leftBelow = this.curView.get(new Coordinate(coor.x-j, coor.y-1));
+                    if (!checkWall(leftBelow)){
+                        //get the diagonal tiles
+                        leftSide = this.curView.get(new Coordinate(coor.x-j-1, coor.y-1));
                     }
-                    if(checkWall(rightTile)){
-                        rightMost = new Coordinate(coor.x+j, coor.y);
-                    }
-                }
-                //get the diagonal tiles
-                MapTile leftSide = this.curView.get(new Coordinate(leftMost.x-1, leftMost.y-1));
-                MapTile rightSide = this.curView.get(new Coordinate(rightMost.x+1, rightMost.y-1));
 
-                return checkTwoSide(leftSide, rightSide);
+                    MapTile rightBelow = this.curView.get(new Coordinate(coor.x+j, coor.y-1));
+                    if (!checkWall(rightBelow)){
+                        rightSide = this.curView.get(new Coordinate(coor.x+j+1, coor.y-1));
+                    }
+
+                }
+
+                if (checkTwoSide(leftSide, rightSide)){
+                    return true;
+                }
             }
         }
         return false;
